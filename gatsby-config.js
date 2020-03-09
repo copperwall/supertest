fb66bd6f11c7d1c515d29e009b2bc1732db596b3
Chris Opperwall <chriso@ifixit.com>
2020-03-08 19:44:59 -0700

    feat: use s3

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,27 @@
 module.exports = {
   siteMetadata: {
-    title: `Client only paths`,
+    title: `Client only paths`
   },
   plugins: [
     {
       resolve: `gatsby-plugin-typography`,
       options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
+        pathToConfigModule: `src/utils/typography`
+      }
     },
     // used to generate rewrites for client only paths
     // on demo hosted on Netlify
     `gatsby-plugin-netlify`,
     {
-      resolve: 'gatsby-plugin-create-client-paths',
-      options: { prefixes: [`/page/*`, `/super/*`] },
+      resolve: "gatsby-plugin-create-client-paths",
+      options: { prefixes: [`/page/*`, `/super/*`] }
+    },
+    {
+      resolve: `gatsby-plugin-s3`,
+      options: {
+        bucketName: "super-test-copperwall",
+        generateMatchPathRewrites: false
+      }
     }
-  ],
-}
+  ]
+};
